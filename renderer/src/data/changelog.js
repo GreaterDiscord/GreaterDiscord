0ae913a4ce620fa0d103e22b46c718a0a0c0bf89
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-07-22 19:44:08 +0200

    Changed the changelog modale content

--- renderer/src/data/changelog.js
+++ renderer/src/data/changelog.js
@@ -1,13 +1,13 @@
 // fixed, improved, added, progress
 export default {
     video: "https://www.youtube.com/embed/evyvq9eQTqA?si=opmzjGjUArT4VLrj&vq=hd720p&hd=1&rel=0&showinfo=0&mute=1&loop=1&autohide=1",
-    description: "A hotfix to get things going again. Plugins and Themes will take time to update.",
+    description: "First release",
     changes: [
         {
-            title: "Bugs Squashed",
-            type: "fixed",
+            title: "New",
+            type: "added",
             items: [
-                "Fixed transition group module search that prevented startup."
+                "Changed `BdApi` to `GdApi` but left an alias."
             ]
         },
     ]
